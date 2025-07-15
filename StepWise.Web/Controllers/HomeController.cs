f14510161a0da2a47f71ff028c93c239f481c9d3
Irena07 <irra.nalbantova@gmail.com>
2025-07-15 14:13:05 +0300

    Updated Home page

--- StepWise.Web/Controllers/HomeController.cs
+++ StepWise.Web/Controllers/HomeController.cs
@@ -21,7 +21,7 @@ namespace StepWise.Web.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int? statusCode)
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
